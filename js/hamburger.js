14d423d1451b5dc3fa533a67296920cffbd3e035
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-03-30 01:12:25 -0600

    basic implementation of hamburger menu on mobile, including jquery anim

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,19 +1,39 @@
+var selector = 'header nav ul li:not(#personal-brand)';
 var menuItems = $('header nav ul li:not(#personal-brand)').get();
 
-setItemVisibility(); //intially called
+setItemVisibilityOnSize(); //intially called
 
 $(window).resize(() => {
-  setItemVisibility();
+  setItemVisibilityOnSize();
 });
 
-function setItemVisibility() {
+function setItemVisibilityOnSize() {
   if($(window).width() > 600) {
     for(let i = 0; i < menuItems.length; i++) {
-      menuItems[i].style.display = 'inline-block';
+      menuItems[i].style.display = 'inline-block'; //this avoids a slideDown() animation on resize
     }
   } else {
     for(let i = 0; i < menuItems.length; i++) {
-      menuItems[i].style.display = 'none';
+      menuItems[i].style.display = 'none'; //this avoids a slideUp() animation on resize
+    }
+  }
+}
+
+$('#hamburger-menu')[0].addEventListener('click', () => {
+  var visible = menuItems[0].style.display != 'none';
+  if(!visible) {
+    toggleVisibility(true);
+  } else {
+    toggleVisibility(false);
+  }
+});
+
+function toggleVisibility(bool) {
+  for(let i = 0; i < menuItems.length; i++) {
+    if(bool) {
+      $(selector).slideDown();
+    } else {
+      $(selector).slideUp();
     }
   }
 }
