37fb173c32fe6e8d55b4d4402cf87d0b47de2f8f
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-03-29 23:02:08 -0600

    added event listeners to deal with card clicks like a normal hyperlink

--- js/cards.js
+++ js/cards.js
@@ -3,10 +3,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
   var images = document.querySelectorAll('.card img');
   for(let i = 0; i < images.length; i++) {
     var card = images[i].parentNode;
-    var src = images[i].src;
-    var output = 'url(' + src + ')';
-    card.style.backgroundImage = output;
+    card.style.backgroundImage = 'url(' + images[i].src + ')';
+    card.style.cursor = 'pointer'
     card.removeChild(images[i]);
+
+    var href = card.children[0].href;
+    console.log(href);
   }
+
+  var gallery = document.getElementById('website-gallery');
+  gallery.addEventListener('click', (e) => {
+    if(e.target.className == 'card') {
+      var link = e.target.getElementsByTagName('a')[0];
+      window.open(link.href, link.target);
+    }
+  });
 
 });
