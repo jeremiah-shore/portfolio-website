0c4c0619051538611d6464c6e7564ea682083475
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-03-30 00:15:37 -0600

    cleaned up back-to-top button implementation

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -1,13 +1,10 @@
-// When the user scrolls down 20px from the top of the document, show the button
 var btt = '#back-to-top';
 var btt_button = $(btt)[0];
 
 btt_button.style.cursor = 'pointer';
-btt_button.style.opacity = '0'; // this is a workaround to using disply:none, as the materlize icon doesn't load until it's displayed
 
 window.onscroll = () => {
   setButtonDisplay();
-  btt_button.style.opacity = 1;
 };
 
 function setButtonDisplay() {
@@ -20,6 +17,10 @@ function setButtonDisplay() {
 }
 
 btt_button.addEventListener('click', () => {
+  scrollToTop();
+});
+
+function scrollToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-});
+}
